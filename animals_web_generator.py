6f752e3f0c0a92032bc5d583fedbbbcd7b5635b1
aslopianka <aslopianka12@gmail.com>
2026-06-03 12:06:04 +0200

    Initial commit with starter code

---
+++ animals_web_generator.py
@@ -0,0 +1,63 @@
+import json
+
+
+def load_data(file_path):
+  """ Loads a JSON file """
+  with open(file_path, "r") as handle:
+    return json.load(handle)
+
+
+def serialize_animal():
+    animals_data = load_data('animals_data.json')
+
+    output_string = ""
+    for animal in animals_data:
+        char = animal.get('characteristics', {})
+        tax = animal.get('taxonomy', {})
+        name = animal.get('name', '-')
+
+        details = {
+            "Scientific Name": tax.get('scientific_name'),
+            "Diet": char.get('diet'),
+            "Location": animal.get('locations', [None])[0],
+            "Type": char.get('type')
+        }
+
+        list_items = ""
+        for label, value in details.items():
+            if value:
+                list_items += f"<li><strong>{label}:</strong> {value}</li>\n"
+
+        output_string += f"""
+                <li class="cards__item">
+                  <div class="card__title">{name}</div>
+                  <div class="card__text">
+                    <ul class="card__list">
+                      {list_items}
+                    </ul>
+                  </div>
+                </li>
+                """
+
+    return output_string
+
+
+def read_html_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+
+
+def write_html_file(file_path, content):
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+
+def main():
+    animal_data = serialize_animal()
+    html_content = read_html_file('animals_template.html')
+    html_content = html_content.replace('__REPLACE_ANIMALS_INFO__', animal_data)
+    write_html_file('animals.html', html_content)
+
+
+if __name__ == "__main__":
+    main()
