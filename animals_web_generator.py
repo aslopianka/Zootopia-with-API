4af03b5e0dedf3fe3ceb6e9cad2e0d435e5bdb25
aslopianka <aslopianka12@gmail.com>
2026-06-03 12:43:50 +0200

    added simple error handling for search term that does not result in status code 2**

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -16,8 +16,9 @@ def fetch_animal_data(animal_search_term):
     api_headers = {"X-Api-Key": API_KEY}
 
     res = requests.get(ANIMALS_API_URL, params=query_params, headers=api_headers)
-    return res.json()
-
+    if res.status_code == 200:
+        return res.json()
+    return []
 
 def serialize_animal(animals_data):
 
@@ -65,7 +66,14 @@ def write_html_file(file_path, content):
 def main():
     animal_search_term = input("Enter the animal you would like to search: ")
     animal_data = fetch_animal_data(animal_search_term)
-    output_string = serialize_animal(animal_data)
+    if not animal_data:
+        output_string = f"""
+            <div style="text-align: center; margin: 40px auto; max-width: 500px;">
+              <h2>The animal "{animal_search_term}" doesn't exist.</h2>
+              <p style="color: #666; font-size: 1.1rem;">Please try a different animal or check your spelling.</p>
+            </div>
+            """
+    else: output_string = serialize_animal(animal_data)
     html_content = read_html_file('animals_template.html')
     html_content = html_content.replace('__REPLACE_ANIMALS_INFO__', output_string)
     write_html_file('animals.html', html_content)
